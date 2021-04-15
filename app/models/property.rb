46804fed9a0f9f51692e35c2f990b788fb592a5f
sachio <sachio.engineer@gmail.com>
2021-04-15 22:14:14 +0900

    Add propertyとstationモデルにバリデーション追加

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,7 @@
 class Property < ApplicationRecord
+  validates :property_name, :rent, :address, presence: true
+  validates: rent, numericality: { only_integer: true }
+  validates :rent, format: { with: /\A[0-9]+\z/ }
   has_many :stations, dependent: :destroy, inverse_of: :property
   accept_nested_attributes_for :stations
 end
