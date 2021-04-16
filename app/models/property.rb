7d81680fec9579c37e8fe48da319cabc1ec335ec
sachio <sachio.engineer@gmail.com>
2021-04-17 00:08:34 +0900

    Add 物件情報のCRUD機能作成

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,7 +1,7 @@
 class Property < ApplicationRecord
   validates :property_name, :rent, :address, presence: true
-  validates: rent, numericality: { only_integer: true }
+  validates :rent, numericality: { only_integer: true }
   validates :rent, format: { with: /\A[0-9]+\z/ }
   has_many :stations, dependent: :destroy, inverse_of: :property
-  accept_nested_attributes_for :stations
+  accepts_nested_attributes_for :stations, allow_destroy: true, reject_if: :all_blank
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'properties#index'
   resources :stations
   resources :properties
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
